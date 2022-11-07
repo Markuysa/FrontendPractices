569caebf9ff6f44fb84e8aea7f0e57c6471ed84d
Markuysa <islam.kus0101@gmail.com>
2022-11-07 22:20:25 +0300

    finished

--- Practice 9/js/main_script.js
+++ Practice 9/js/main_script.js
@@ -62,6 +62,36 @@ secondButton.addEventListener('click',secondTask);
 
 //------------------------------------------------------------------------------------------------------------------//
 let count=0;
+var mouseX = 0, mouseY = 0;
+
+const mousemove = ()=>{
+    addEventListener('mousemove',(e)=>{
+    var div = document.createElement("div");
+    div.style.width = "64px";
+    div.style.height = "64px";
+    div.style.borderRadius = "50%";
+    
+    div.style.backgroundImage = "url('/img/icons8-червы-64.png')";
+    div.style.backgroundRepeat = "no-repeat";
+    div.style.backgroundPosition = "0";
+        
+    div.style.color = "white";
+    // div.innerHTML = "Hello";
+    div.style.position="absolute"
+    mouseX = e.pageX;
+    mouseY = e.pageY; 
+    div.style.left = mouseX+20+'px'
+    div.style.top = mouseY+20+'px'
+    
+    setTimeout(() => {
+        document.body.appendChild(div)    
+    }, 1);
+    setTimeout(() => {
+        document.body.removeChild(div)    
+    }, 1000);
+})
+}
+let like=document.querySelector(".like_button")
 const like_button = ()=>{
     let counter=document.querySelector(".counter")
     let heart = document.querySelector('.heart')
@@ -71,13 +101,16 @@ const like_button = ()=>{
     }else {
         counter.textContent = String(Number(counter.textContent) - 1) 
         heart.classList.remove("active");
+        like.removeEventListener('click',mousemove)
     }
     count++;
 }
 
-let like=document.querySelector(".like_button")
 like.addEventListener('click',like_button)
+like.addEventListener('click',mousemove)
 
 
+
+//---------------------------------------------------------------------------------------------------------------------//
 
 
